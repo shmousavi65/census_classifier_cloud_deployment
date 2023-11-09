65491bc8933e1f41ce9fa4fc229de8a39222455f
hossein mousavi <s.h.mousavi65@gmail.com>
2023-11-09 15:41:33 -0500

    add data_check component for the pipeline

---
+++ starter/starter/data_check/conftest.py
@@ -0,0 +1,11 @@
+import pytest
+import pandas as pd
+
+def pytest_addoption(parser):
+    parser.addoption("--data_path", action="store")
+
+@pytest.fixture(scope="session")
+def data(request):
+    data_path = request.config.option.data_path
+    df = pd.read_csv(data_path)
+    return df
---
+++ starter/starter/data_check/test_data.py
@@ -0,0 +1,46 @@
+import pandas as pd
+import logging
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
+logger = logging.getLogger()
+
+def test_feature_presence(data):
+    '''assert the features used for training are correct'''
+    ref_features = {'age',
+    'workclass',
+    'fnlgt',
+    'education',
+    'education-num',
+    'marital-status',
+    'occupation',
+    'relationship',
+    'race',
+    'sex',
+    'capital-gain',
+    'capital-loss',
+    'hours-per-week',
+    'native-country'}
+
+    data_features = set(data.columns)
+    data_features.remove('salary')
+    assert data_features == ref_features
+
+
+def test_age_limit(data):
+    '''assert the age is in the current range'''
+    assert data.age.between(0,122).all()
+
+def test_workclass_classes(data):
+    '''assert the classes in the workclasses feature'''
+    assert set(data.workclass.unique()) == {' State-gov', 
+                                            ' Self-emp-not-inc',
+                                            ' Private',
+                                            ' Federal-gov',
+                                            ' Local-gov',
+                                            ' ?',
+                                            ' Self-emp-inc',
+                                            ' Without-pay',
+                                            ' Never-worked'}
+
+
+
