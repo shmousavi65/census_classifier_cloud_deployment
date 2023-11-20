5b7e0afa8b95e1ecec45b615be811519500abfef
hossein mousavi <s.h.mousavi65@gmail.com>
2023-11-20 13:47:32 -0500

    - modify config file and track - remote starter dir name to development

--- development/pipeline/pipeline.py
+++ development/pipeline/pipeline.py
@@ -9,6 +9,8 @@ from omegaconf import DictConfig, OmegaConf
 def go(config: DictConfig):
 
     root_path = hydra.utils.get_original_cwd()
+    data_model_parent_dir = os.path.dirname(os.path.dirname(root_path))    
+    print("HEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE", os.path.join(data_model_parent_dir))
 
     exec_steps_list = list(config["execution_steps"])
 
@@ -17,7 +19,7 @@ def go(config: DictConfig):
                 os.path.join(root_path, "data_check"),
                 "main",
                 parameters={
-                    "data_path": config["data"]["raw_data_path"]
+                    "data_path": os.path.join(data_model_parent_dir, config["data"]["raw_data_path"])
                 },
                 env_manager="local"
             )
@@ -27,7 +29,7 @@ def go(config: DictConfig):
                 os.path.join(root_path, "split_data"),
                 "main",
                 parameters={
-                    'raw_data_path' : config["data"]["raw_data_path"],
+                    'raw_data_path' : os.path.join(data_model_parent_dir, config["data"]["raw_data_path"]),
                     'random_state' : config['train']['random_state'],
                     'test_size' :  config['train']['test_size'],
                 },
@@ -35,7 +37,7 @@ def go(config: DictConfig):
             )
     
     if "model_train" in exec_steps_list:
-        raw_data_path = config["data"]["raw_data_path"]
+        raw_data_path = os.path.join(data_model_parent_dir, config["data"]["raw_data_path"])
         raw_dirname = os.path.dirname(raw_data_path)
         raw_basename = os.path.basename(raw_data_path)
         train_basename = raw_basename.split(".")[0] + "_train.csv"  
@@ -48,14 +50,14 @@ def go(config: DictConfig):
                     "numerical_features": config['train']['num_features'],
                     "categorical_features": config['train']['cat_features'],
                     "model_params":config['train']['model_params'],
-                    'model_save_path': config['model']['model_save_path'],
+                    'model_save_path': os.path.join(data_model_parent_dir, config["model"]["model_save_path"]),
                     'output_label': config['train']['output_label']
                 },
                 env_manager="local"
             )
 
     if "performace_eval" in exec_steps_list:
-        raw_data_path = config["data"]["raw_data_path"]
+        raw_data_path = os.path.join(data_model_parent_dir, config["data"]["raw_data_path"])
         raw_dirname = os.path.dirname(raw_data_path)
         raw_basename = os.path.basename(raw_data_path)
         test_basename = raw_basename.split(".")[0] + "_test.csv"  
@@ -65,7 +67,7 @@ def go(config: DictConfig):
                 "main", 
                 parameters={
                     'data_path' : test_data_path,
-                    'model_path': config['model']['model_save_path'],
+                    'model_path': os.path.join(data_model_parent_dir, config["model"]["model_save_path"]),
                     'output_label': config['train']['output_label'],
                     'slice_eval_features': config['evaluation']['slice_eval_features']
                 },
