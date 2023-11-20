2eee2fefa8cd8306c3c580b3df6bc4bc3ad19f89
hossein mousavi <s.h.mousavi65@gmail.com>
2023-11-20 10:30:52 -0500

    write app body and test

--- starter/main.py
+++ starter/main.py
@@ -1,1 +1,52 @@
-# Put the code for your API here.
+import itertools 
+import pandas as pd
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel, create_model
+from starter.ml.model import load_model, inference
+
+# parameters
+path_to_model = "model/model.pkl"
+path_to_example_df = "data/census.csv"
+example_df_index = 0
+
+# load the model
+input_pipe, output_transformer = load_model(path_to_model)
+used_columns = list(itertools.chain.from_iterable([x[2] for x in input_pipe['preprocessor'].transformers]))
+used_columns_set = set(used_columns)
+
+#load an example
+example_df = pd.read_csv(path_to_example_df)
+example_series = example_df.iloc[example_df_index]
+example_series = example_series[used_columns]
+example_dict = example_series.to_dict()
+
+class InputItem(BaseModel):
+    element : dict
+
+    model_config = {
+        "json_schema_extra": {
+            "examples": [
+                example_dict
+            ]
+        }
+    }
+
+# create app instance
+app = FastAPI()
+
+@app.get("/")
+async def do_greeting():
+    return "Hello User!"
+
+@app.post("/inference/")
+async def do_inference(input: InputItem):
+    input_keys = set(input.element.keys())
+    if used_columns_set.difference(input_keys):
+        raise HTTPException(status_code=191, detail="The input does not contain the required values!")
+    
+    input_df = pd.DataFrame.from_dict(input.element, orient='index').transpose()
+    if len(input_df) != 1:
+        raise HTTPException(status_code=193, detail="The input does should only contain 1 sample!")
+    
+    output = inference(input_pipe, input_df)
+    return {"output": int(output[0])}
---
+++ starter/test_main.py
@@ -0,0 +1,43 @@
+import itertools 
+import pandas as pd
+from fastapi.testclient import TestClient
+from starter.ml.model import load_model, inference
+
+from main import app
+from main import InputItem
+# parameters
+path_to_model = "model/model.pkl"
+path_to_example_df = "data/census.csv"
+example_df_index = 0
+
+# load an example input
+input_pipe, output_transformer = load_model(path_to_model)
+used_columns = list(itertools.chain.from_iterable([x[2] for x in input_pipe['preprocessor'].transformers]))
+used_columns_set = set(used_columns)
+
+example_df = pd.read_csv(path_to_example_df)
+example_series = example_df.iloc[example_df_index]
+example_series = example_series[used_columns]
+example_dict = example_series.to_dict()
+
+
+client = TestClient(app)
+
+def test_api_locally_do_greeting():
+    r = client.get("/")
+    assert r.status_code == 200
+    assert r.json() == "Hello User!"
+
+def test_api_locally_post():
+    input_dict = {"element": example_dict}
+    r = client.post("/inference/", json=input_dict)
+    
+    out = r.json()
+    assert r.status_code == 200 # assert response code is correct
+    assert out["output"] == 0 # assert inference result is correct
+
+def test_api_locally_post_wrong_input():
+    example_dict.pop("age")
+    input_dict = {"element": example_dict}
+    r = client.post("/inference/", json=input_dict)
+    assert r.status_code == 191 # assert response code is correct
