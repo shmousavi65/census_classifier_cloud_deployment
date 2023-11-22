a6fdecba43cb2eecfc26283d3d9217da5ae8ec6e
hossein mousavi <s.h.mousavi65@gmail.com>
2023-11-22 16:02:40 -0500

    update based on flake8

--- development/sanitycheck.py
+++ development/sanitycheck.py
@@ -64,9 +64,9 @@ def run_sanity_check(test_dir):
                 TEST_FOR_GET_METHOD_RESPONSE_BODY = True
 
         if not TEST_FOR_GET_METHOD_RESPONSE_CODE:
-            print(FAIL_COLOR+f"[{WARNING_COUNT}]")
+            print(FAIL_COLOR + f"[{WARNING_COUNT}]")
             WARNING_COUNT += 1
-            print(FAIL_COLOR+"Your test case for GET() does not seem to be \
+            print(FAIL_COLOR + "Your test case for GET() does not seem to be \
                    testing the response code.\n")
 
         if not TEST_FOR_GET_METHOD_RESPONSE_BODY:
@@ -83,7 +83,8 @@ def run_sanity_check(test_dir):
     if not test_functions_for_post:
         print(FAIL_COLOR + f"[{WARNING_COUNT}]")
         WARNING_COUNT += 1
-        print(FAIL_COLOR + "No test cases were detected for the POST() method.")
+        print(FAIL_COLOR + "No test cases were detected for the POST() \
+               method.")
         print(FAIL_COLOR + "Please make sure you have TWO test cases for the \
             POST() method." +
               "\nOne test case for EACH of the possible inferences \
@@ -118,14 +119,14 @@ def run_sanity_check(test_dir):
         if not TEST_FOR_POST_METHOD_RESPONSE_BODY:
             print(FAIL_COLOR + f"[{WARNING_COUNT}]")
             WARNING_COUNT += 1
-            print(FAIL_COLOR+"One or more of your test cases for POST() do \
+            print(FAIL_COLOR + "One or more of your test cases for POST() do \
                   not seem to be testing the contents of the response.\n")
 
         if len(test_functions_for_post) >= 2 and \
                 COUNT_POST_METHOD_TEST_FOR_INFERENCE_RESULT < 2:
             print(FAIL_COLOR + f"[{WARNING_COUNT}]")
             WARNING_COUNT += 1
-            print(FAIL_COLOR+"You do not seem to have TWO separate test \
+            print(FAIL_COLOR + "You do not seem to have TWO separate test \
                 cases, one for each possible prediction that your model \
                  can make.")
 
